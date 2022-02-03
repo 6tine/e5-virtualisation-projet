07a9787355f558b35eb5518ec37b2ebe1bdc4367
traverss <sixtine.traverse@edu.esiee.fr>
2022-02-03 19:47:01 +0100

    init projet api

---
+++ virtualisation-project-api/src/main/java/com/esiee/virtualisationprojectapi/VirtualisationProjectApiApplication.java
@@ -0,0 +1,13 @@
+package com.esiee.virtualisationprojectapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualisationProjectApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtualisationProjectApiApplication.class, args);
+	}
+
+}
---
+++ virtualisation-project-api/src/test/java/com/esiee/virtualisationprojectapi/VirtualisationProjectApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esiee.virtualisationprojectapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualisationProjectApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
